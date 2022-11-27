0b725dc613b7ce5820006876d24de5d38ee60063
NgocLong7901 <111212028+NgocLong7901@users.noreply.github.com>
2022-11-27 09:13:46 +0700

    Update QuanlychitieucanhanApplication.java

--- quanlychitieucanhan/src/main/java/vn/huce/da/ltudm/quanlychitieucanhan/QuanlychitieucanhanApplication.java
+++ quanlychitieucanhan/src/main/java/vn/huce/da/ltudm/quanlychitieucanhan/QuanlychitieucanhanApplication.java
@@ -9,5 +9,15 @@ public class QuanlychitieucanhanApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(QuanlychitieucanhanApplication.class, args);
 	}
+	
+	@Bean
+    	public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+            }
+        };         
+   	}
 
 }
